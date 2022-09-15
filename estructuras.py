2f5de41dc2b55758f250c14abf972ca7032b9585
JorgVargas <jorge.vargas2406@outlook.com>
2022-09-14 19:40:55 -0500

    se hizo ejercicio de estructuras

---
+++ estructuras.py
@@ -0,0 +1,11 @@
+# EL comando IF
+
+num= int(input("Ingrese un numeero:\n"))
+
+if num == 100:
+    print("Usted escribio el 100")
+elif num > 100:
+    print("usted escribio un numero mayor a 100")
+
+else:
+        print("usted escribio un numero menor a 100")
